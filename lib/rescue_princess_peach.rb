0fe7efa63eafc830c2ba3e796c260e2cf8085d00
Toron Seigler <toronseigler9@gmail.com>
2019-10-10 00:58:47 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -2,4 +2,8 @@ def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
-ruby lib/rescue_princess_peach.rb
+
+
+
+#rescue_princess_peach
+#puts status
